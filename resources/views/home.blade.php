6e5c0ffa01ebbb077cf754613b21e909c7eefa50
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-09-23 00:57:07 +0200

    added view home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('main-content')
+
+<div class="row">
+
+    <div class="col-12 d-flex align-items-center justify-content-center">
+        <a href="{{ route('comics.index') }}" class="btn btn-success btn-lg">Guarda i comics</a>
+    </div>
+
+</div>
+
+@endsection
